e01d1a6af0d97eaca090dbb4a0ae9d5c5a132afd
sissy5656 <supersissy5656@gmail.com>
2020-08-30 13:33:46 -0500

    studying

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-
 from .models import Question, Choice
 
-admin.site.register(Question)
+admin.site.site_header = "Django App"
+admin.site.site_title = "Django App Area"
+admin.site.index_title = "Welcome to this Django App"
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {"fields": ["question_text"]}), ("Date Information", {
+        "fields": ["pub_date"], "classes":["collapse"]}), ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question, Choice
+
+
+def index(request):
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    context={'latest_question_list':latest_question_list}
+    return render(request, "polls/index.html",context)
+
+
+def details(request,question_id):
+    try:
+        question=Question.object.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'polls/results.html',{'question':question})
